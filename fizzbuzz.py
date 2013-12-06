3086c74704df7f23e0b1ea527b1381560335822e
Justin Perez <perezstr@yahoo.com>
2013-12-06 08:24:40 -0600

    This should finish fizzbuzz
    @perezjust please review...will I message myself?

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,11 +6,12 @@ x = 0
 for i in range(0,100):
     x += 1.0
     y = str(x / 3.0)
-    if y.split(".")[1] == "0" and str(x).split(".")[0].endswith("5") or str(x).split(".")[0].endswith("0"):
-        print "fizzbuzz - " + str(x)
-    elif y.split(".")[1] == "0":
+    z = str(x / 5.0)
+    if y.split(".")[1] == "0" and z.split(".")[1] == "0":
+        print "fizzbuzz"
+    elif y.split(".")[1] == "0" and z.split(".")[1] != "0":
         print "fizz"
-    elif str(x).split(".")[0].endswith("5") or str(x).split(".")[0].endswith("0"):
-        print "buzz - " + str(x).split(".")[0]
+    elif y.split(".")[1] != "0" and z.split(".")[1] == "0":
+        print "buzz"
     else:
-        print x
+        print x, y, z
